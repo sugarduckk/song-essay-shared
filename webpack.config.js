7735fcaedde4dcdf5dce8c8b3eb7c369eb4c963a
Aung <potcharaphol.c@gmail.com>
2020-05-30 21:15:13 +0700

    Auto copy package.json to lib folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,15 @@
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
 module.exports = {
   entry: {
-    main: './src/index.js',
-    layouts: './src/layouts/index.js',
-    hooks: './src/hooks/index.js',
+    'main': './src/index.js',
+    'layouts': './src/layouts/index.js',
+    'hooks': './src/hooks/index.js',
     'form-items': './src/form-items/index.js',
-    buttons: './src/buttons/index.js'
+    'buttons': './src/buttons/index.js',
+    'screens': './src/screens/index.js'
   },
   output: {
     path: path.resolve(__dirname, 'lib'),
@@ -15,11 +17,17 @@ module.exports = {
     library: 'song-essay-shared',
     libraryTarget: 'umd'
   },
-  // plugins: [
-  //   new CleanWebpackPlugin()
-  // ],
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [
+        { from: 'copy', to: '' }
+      ],
+    }),
+  ],
   optimization: {
     splitChunks: {
+      name: 'vendors',
       chunks: 'all',
     },
     runtimeChunk: 'single'
@@ -47,5 +55,6 @@ module.exports = {
   externals: {
     'react': 'react',
     'react-dom': 'react-dom'
-  }
+  },
+  devtool: 'inline-source-map',
 };
